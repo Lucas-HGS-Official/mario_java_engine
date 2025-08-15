9240f32c7fccc2cbf24aa0b0d747abcd541c841e
LhgS <lucas.henrique.gois.santos@gmail.com>
2025-08-14 23:41:49 -0300

    Ligando o callback para o teclado ao glfw

--- src/main/java/Jade/Window.java
+++ src/main/java/Jade/Window.java
@@ -62,6 +62,7 @@ public class Window {
         glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
         glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
         glfwMakeContextCurrent(glfwWindow);
         glfwSwapInterval(1);
